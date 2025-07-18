011af2fc0636f28cb1dfe08c650ba83ae5e9df70
kiongosss <user@example.com>
2025-07-18 14:35:01 +0300

    Update footer and license with Omosayansi Moka's name

--- src/lib/content/footer.ts
+++ src/lib/content/footer.ts
@@ -1,6 +1,6 @@
 import { FooterSectionType } from '@/lib/types/sections';
 
 export const footerSection: FooterSectionType = {
-  title: 'Design & Built by Vatsal Singh',
-  link: 'https://github.com/vatsalsinghkv/portfolio-website',
+  title: 'Design & Built by Omosayansi Moka',
+  link: 'https://omosayansi-portfolio.vercel.app/ ',
 };
